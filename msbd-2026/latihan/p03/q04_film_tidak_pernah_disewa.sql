74dfb1a2542569e62ea904d70a3d286fa60dcc4e
fityashofiahmad@gmail.com <fityashofiahmad@gmail.com>
2026-09-08 22:02:25 +0700

    nambah q04_film_tidak_pernah_disewa.sql

--- msbd-2026/latihan/p03/q04_film_tidak_pernah_disewa.sql
+++ msbd-2026/latihan/p03/q04_film_tidak_pernah_disewa.sql
@@ -6,3 +6,13 @@ WHERE film_id NOT IN (
     FROM inventory i
     JOIN rental r ON i.inventory_id = r.inventory_id
 );
+
+-- versi NOT EXISTS --
+SELECT f.title
+FROM film f
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM inventory i
+    JOIN rental r ON i.inventory_id = r.inventory_id
+    WHERE i.film_id = f.film_id
+);
